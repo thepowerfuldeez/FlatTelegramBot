642a96f23591213314ad2f0e2c2091fc440ef3a1
Georgiy <deezchannel@gmail.com>
2018-03-17 16:40:07 +0300

    new rules for vk requests

--- vk_module.py
+++ vk_module.py
@@ -1,10 +1,10 @@
-import vk
+import vk_api
 import os
 
-vk_session = vk.Session(access_token=os.environ.get("VK_TOKEN"))
-vk = vk.API(vk_session)
+vk_session = vk_api.VkApi(token=os.environ.get("VK_TOKEN"))
+vk = vk_session.get_api()
 
 
 def get_public_updates(public_id, n=20, offset=0):
     posts = vk.wall.get(owner_id=-public_id, count=n, offset=offset, v=5.71)
-    return posts[1:]
+    return posts['items']
