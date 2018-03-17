c5969afbea72d990ca506a9eab9780e94dfa436f
Georgiy <deezchannel@gmail.com>
2018-03-17 16:34:07 +0300

    change vk request

--- vk_module.py
+++ vk_module.py
@@ -6,5 +6,5 @@ vk = vk.API(vk_session)
 
 
 def get_public_updates(public_id, n=20, offset=0):
-    posts = vk.wall.get(owner_id=-public_id, count=n, offset=offset)
+    posts = vk.wall.get(owner_id=-public_id, count=n, offset=offset, v=5.71)
     return posts[1:]
